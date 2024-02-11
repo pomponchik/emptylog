1e0062db5de28c9f35446c64fbb86878d3a89a76
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-11 19:27:32 +0300

    tests

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -8,3 +8,8 @@ def test_positive_examples_of_runtime_check():
     assert isinstance(logging.getLogger('kek'), LoggerProtocol)
     assert isinstance(logging.getLogger('kek'), LoggerProtocol)
     assert isinstance(EmptyLogger(), LoggerProtocol)
+
+
+def test_negative_examples_of_runtime_check():
+    assert not isinstance(1, LoggerProtocol)
+    assert not isinstance('logging', LoggerProtocol)
