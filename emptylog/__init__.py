3c5fde87a530e291bafa99c8acf4296d9787f945
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-13 17:43:57 +0300

    import path

--- emptylog/__init__.py
+++ emptylog/__init__.py
@@ -1,2 +1,3 @@
 from emptylog.empty_logger import EmptyLogger as EmptyLogger  # noqa: F401
 from emptylog.protocol import LoggerProtocol as LoggerProtocol  # noqa: F401
+from emptylog.memory_logger import MemoryLogger as MemoryLogger  # noqa: F401
