f66ce386b3ddc9c3b70d74537a0b482c4299487a
Evgeniy Blinov <zheni-b@yandex.ru>
2024-07-08 19:28:24 +0300

    added PrintingLogger + tests

---
+++ tests/test_printing_logger.py
@@ -0,0 +1,55 @@
+import io
+import re
+from contextlib import redirect_stdout
+
+import pytest
+
+from emptylog import PrintingLogger, LoggerProtocol
+
+
+def test_printing_logger_is_logger():
+    assert isinstance(PrintingLogger(), LoggerProtocol)
+
+
+@pytest.mark.parametrize(
+    ['method', 'result_tail'],
+    (
+        (PrintingLogger().debug, ' | DEBUG     | kek'),
+        (PrintingLogger().info, ' | INFO      | kek'),
+        (PrintingLogger().warning, ' | WARNING   | kek'),
+        (PrintingLogger().error, ' | ERROR     | kek'),
+        (PrintingLogger().exception, ' | EXCEPTION | kek'),
+        (PrintingLogger().critical, ' | CRITICAL  | kek'),
+
+        (PrintingLogger(separator='*').debug, ' * DEBUG     * kek'),
+        (PrintingLogger(separator='*').info, ' * INFO      * kek'),
+        (PrintingLogger(separator='*').warning, ' * WARNING   * kek'),
+        (PrintingLogger(separator='*').error, ' * ERROR     * kek'),
+        (PrintingLogger(separator='*').exception, ' * EXCEPTION * kek'),
+        (PrintingLogger(separator='*').critical, ' * CRITICAL  * kek'),
+    ),
+)
+def test_check_simple_output(method, result_tail):
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        method('kek')
+
+    printed = buffer.getvalue()
+
+    assert printed.endswith(result_tail + '\n')
+
+    time_stamp = printed.removesuffix(result_tail + '\n') # expected format: 2024-07-08 19:09:48.226667
+
+    assert len(time_stamp.split()) == 2
+
+    date = time_stamp.split()[0]
+    time = time_stamp.split()[1]
+
+    assert re.match(r'[\d]{4}-[\d]{2}-[\d]{2}', date) is not None
+
+    time_before_dot = time.split('.')[0]
+    time_after_dot = time.split('.')[1]
+
+    assert len(time.split('.')) == 2
+    assert re.match(r'[\d]{2}:[\d]{2}:[\d]{2}', time_before_dot) is not None
+    assert time_after_dot.isdigit()
